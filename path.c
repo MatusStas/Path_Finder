0790b76605f851e7a41f4c7ee0941e5bd15af43b
Matus Stas <matusstas.ms@gmail.com>
2018-12-18 18:50:26 +0100

    added funtion to load matrix from FILE and second function that is able to show loaded matrix

--- path.c
+++ path.c
@@ -13,8 +13,43 @@ void size_of_grid(int *row, int *column)
 		(sign != '\n') ? total++ : (*row)++;
 
 	(*column) = total/(*row);
-	close(fr);								//always need to close file and initialize to NULL
+	
+	fclose(fr);								//always need to close file and initialize to NULL
 	fr = NULL;
+}
+
+
+void load_matrix(int *row, int *column, char *arr[])
+{
+	int sign,
+		i = 0,
+		j = 0;
+
+	arr[i] = (char *) malloc((*column) * sizeof(char));		//allocing memory for array of pointers
+
+	FILE *fr = fopen("grid.txt","r");
+	while((sign = getc(fr)) != EOF)
+		{
+			if(sign != '\n')
+			{	
+				(*(*(arr + i) + j++)) = sign;
+			}
+			else
+				{
+					arr[++i] = (char *) malloc((*column) * sizeof(char));
+					j = 0;
+				}
+		}
+}
+
+void print_matrix(int *row, int *column, char *arr[])
+{
+	for(int i = 0; i < (*row); i++)
+	{
+		for(int j = 0; j < (*column); j++)
+			printf("%c", *(*(arr + i) + j));
+		putchar('\n');
+	}
 }
 
 
@@ -22,16 +57,20 @@ int main()									//main program
 {
 	char sign;
 	int row = 0, column = 0;
-	char *arr[row];
+	int size;
+	char *arr[size];
 
 	while(scanf("%c",&sign) == 1)			//while we will type signs to run functions
 		{
 			if(sign == 'q')	
 				return 0;					//quit the program
-			else if(sign == 's')
+			else if(sign == 'l')
 			{
-				size_of_grid(&row,&column);
+				size_of_grid(&row, &column);
+				load_matrix(&row, &column,&*arr);
 			}
+			else if(sign == 'p')
+				print_matrix(&row, &column, &*arr);
 		}
 
 }
