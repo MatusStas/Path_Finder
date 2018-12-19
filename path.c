504f576627f34c12f264975c9e92dc0f63e29a01
Matus Stas <matusstas.ms@gmail.com>
2018-12-19 22:27:32 +0100

    added function  find_point

--- path.c
+++ path.c
@@ -89,7 +89,30 @@ void print_matrix(int *row, int *column, int *arr[])
 }
 
 
+int find_point(int *point, int *row, int *column, int *arr[])	
+{
+	int i,
+		j,
+		character;
 
+	scanf("%d", &point[0]);
+
+
+	for(i = 0; i < (*row); i++)
+	{
+		for(j = 0; j < (*column); j++)
+		{
+			character = *(*(arr + i) + j);
+			if(*(point + 0)== character)
+				{
+					*(point + 1) = i;	
+					*(point + 2) = j;
+					return 0;
+				}
+		}
+	}
+	return 0;
+}
 
 int main()									//main program
 {
@@ -97,9 +120,11 @@ int main()									//main program
 	int row = 0, column = 0;
 	int size;
 	int *arr[size];
-
-
-
+	int *point = (int *) malloc(7*sizeof(int));	// first position what character
+												// second position x location
+												// third postion y location
+												// 4 - 7 neighbours if obstacle or doesn't exist set to -2
+ 
 	while(scanf("%c",&sign) == 1)			//while we will type signs to run functions
 		{
 			if(sign == 'q')	
@@ -111,6 +136,11 @@ int main()									//main program
 			}
 			else if(sign == 'p')
 				print_matrix(&row, &column, &*arr);
-		}
+			else if(sign == 'f')
+			{
+				find_point(point, &row, &column, &*arr);
+				//printf("%d %d %d\n", *(point + 0),*(point + 1),*(point + 2));
+			}
 
+		}
 }
